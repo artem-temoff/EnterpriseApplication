4b34763b794d1bcf5116338bba449b6825175406
artem-temoff <artem-temoff@mail.ru>
2014-11-04 16:49:33 +0300

    Second

--- EnterpriseApplication1-ejb/src/java/com/EnterpriseApplication1/jpa/Users.java
+++ EnterpriseApplication1-ejb/src/java/com/EnterpriseApplication1/jpa/Users.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.EnterpriseApplication.jpa;
+package com.EnterpriseApplication1.jpa;
 
 import java.io.Serializable;
 import javax.faces.bean.RequestScoped;
